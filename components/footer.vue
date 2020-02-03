9edc8b970c24c4da3d9afe80ca4a7354e396fb96
jakubgania <contact@jakubgania.io>
2020-02-04 00:46:10 +0100

    created footer component

---
+++ components/footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div>
+      OKNA - SYSTEM - SERWIS | &copy; 2017 - 2020
+    </div>
+    <div>
+      wykonanie -
+      <a href="https://jakubgania.io">jakubgania.io</a>
+    </div>
+  </div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,20 @@
 <template>
   <div>
     <nuxt />
+
+    <footer-component />
   </div>
 </template>
+
+<script>
+import FooterComponent from '@/components/footer'
+
+export default {
+  components: {
+    FooterComponent
+  }
+}
+</script>
 
 <style>
 /*  */
