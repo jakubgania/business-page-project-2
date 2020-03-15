3fd1184e3f998f4be7aa312d60f472189cfa9242
jakubgania <contact@jakubgania.io>
2020-03-15 22:09:19 +0100

    updated footer component

--- components/footer.vue
+++ components/footer.vue
@@ -2,11 +2,13 @@
   <div class="footer-wrapper">
     <div class="container">
       <div class="footer-content">
-        <div>
-          OKNA - SYSTEM - SERWIS | &copy; 2017 - 2020
-        </div>
-        <div class="ml-auto">
-          Wykonanie -
+        <span style="color: #fff;font-weight: 600;">
+          &copy; 2017 - 2020 OKNA - SYSTEM - SERWIS
+        </span>
+        <div class="ml-auto author-section">
+          <span style="color: #fff;">
+            Wykonanie -
+          </span>
           <a href="https://jakubgania.io" class="creator-link">
             jakubgania.io
           </a>
@@ -18,26 +20,42 @@
 
 <style lang="scss" scope>
 .footer-wrapper {
-  background-color: #ebf0fa;
-  padding-top: 40px;
-  padding-bottom: 40px;
+  background-color: #000;
+  padding-top: 80px;
+  padding-bottom: 80px;
+}
+.container {
+  padding-left: 25px;
+  padding-right: 25px;
 }
 .footer-content {
   display: flex;
 }
 .creator-link {
   color: #0066ff;
+  font-weight: 600;
+  transition: color 0.2s ease;
 
   &:hover {
-    color: #000;
+    color: #ff6666;
     transition: color 0.2s ease;
     text-decoration: none;
+  }
+}
+
+@media only screen and (max-width: 768px) {
+  .footer-content {
+    font-size: 14px;
   }
 }
 
 @media only screen and (max-width: 576px) {
   .footer-content {
     display: block;
+    text-align: center;
+  }
+  .author-section {
+    margin-top: 10px;
   }
 }
 </style>
